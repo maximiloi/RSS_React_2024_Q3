07229260e7179f689d598395f9366f1150c66483
maximiloi <maximiloi@gmail.com>
2024-07-02 10:42:43 +0300

    file: husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-shadow */
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '../../../../../../../vite.svg';
@@ -18,7 +19,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button type="button" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
