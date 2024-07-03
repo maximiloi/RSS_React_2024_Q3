43c00f1bc0158f1ee817192b0eb852edb7ad4d75
maximiloi <maximiloi@gmail.com>
2024-07-03 18:24:21 +0300

    refactor: utils script

--- src/utils/apiResponse.ts
+++ src/utils/apiResponse.ts
@@ -7,13 +7,14 @@ interface Response {
 
 class ApiResponse {
   static async fetchData(value: string): Promise<Response[]> {
-    const url = `https://www.omdbapi.com/?s=${value}&apikey=67e1bb9b`;
-
-    const response = await fetch(url);
-    const data = await response.json();
-    const results = await data.Search;
-
-    return results;
+    try {
+      const url = `https://www.omdbapi.com/?s=${value}&apikey=67e1bb9b`;
+      const response = await fetch(url);
+      const data = await response.json();
+      return data.Search || [];
+    } catch (error) {
+      throw new Error(`Ошибка при получении данных: ${error}`);
+    }
   }
 }
 
--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -3,7 +3,7 @@ class LocalStorage {
     if (localStorage.getItem('name-cinema-iloi')) {
       return localStorage.getItem('name-cinema-iloi');
     }
-    return localStorage.setItem('name-cinema-iloi', '');
+    return localStorage.setItem('name-cinema-iloi', 'star wars');
   }
 
   static setResult(inputValue: string) {
