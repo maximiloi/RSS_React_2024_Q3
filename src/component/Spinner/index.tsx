c0b1dd0c94989d5fc72f91c8e4c11ab7a8a832dc
maximiloi <maximiloi@gmail.com>
2024-07-12 19:03:48 +0300

    refactor: spinner and Error button component

--- src/component/Spinner/index.tsx
+++ src/component/Spinner/index.tsx
@@ -1,16 +1,13 @@
-import { Component } from 'react';
 import spinnerImg from '../../assets/cinema.png';
 
 import './style.scss';
 
-class Spinner extends Component {
-  render() {
-    return (
-      <div className="spinner">
-        <img className="spinner__img" src={spinnerImg} alt="loading..." />
-      </div>
-    );
-  }
+function Spinner() {
+  return (
+    <div className="spinner">
+      <img className="spinner__img" src={spinnerImg} alt="loading..." />
+    </div>
+  );
 }
 
 export default Spinner;
