5e36001ed12bafe62daf74c0bffe700eeb9efb77
maximiloi <maximiloi@gmail.com>
2024-07-13 13:04:30 +0300

    feat: add search params

--- src/component/Search/index.tsx
+++ src/component/Search/index.tsx
@@ -1,20 +1,32 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import useLocalStorage from '../../hooks/useLocalStorage';
 
 import './style.scss';
 
 function Search() {
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
   const [inputValue, setInputValue] = useState<string>('');
   const [, setValue] = useLocalStorage();
 
   const handleSearch = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     setValue(inputValue);
+    searchParams.set('search', inputValue);
+    navigate(`?${searchParams.toString()}`);
   };
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
   };
+
+  useEffect(() => {
+    const query = searchParams.get('search');
+    if (!query) return;
+    setValue(query);
+    setInputValue(query);
+  }, [searchParams, setValue]);
 
   return (
     <form className="search" onSubmit={handleSearch}>
