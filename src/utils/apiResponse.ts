f41dd8c29626ace3371ee5c23a8c1b2b87f20cbd
maximiloi <maximiloi@gmail.com>
2024-07-14 21:24:17 +0300

    refactor:  all ResponseData

--- src/utils/apiResponse.ts
+++ src/utils/apiResponse.ts
@@ -1,16 +1,23 @@
-export interface MovieData {
-  imdbID: string;
+export interface Movie {
   Title: string;
-  Poster: string;
   Year: string;
+  imdbID: string;
+  Type: string;
+  Poster: string;
 }
 
-const fetchData = async (value: string): Promise<MovieData[]> => {
+interface ResponseData {
+  Search: Movie[];
+  totalResults: string;
+  Response: 'True' | 'False';
+}
+
+const fetchData = async (value: string): Promise<ResponseData> => {
   try {
     const url = `https://www.omdbapi.com/?s=${value}&apikey=67e1bb9b`;
     const response = await fetch(url);
     const data = await response.json();
-    return data.Search || [];
+    return data || [];
   } catch (error) {
     throw new Error(`Ошибка при получении данных: ${error}`);
   }
