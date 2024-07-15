4c444ffd7866b1ff727b2e0abee157e6984f412c
maximiloi <maximiloi@gmail.com>
2024-07-15 22:44:42 +0300

    feat: useEffect pagination

--- src/component/Pagination/index.tsx
+++ src/component/Pagination/index.tsx
@@ -1,4 +1,6 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+
 import './style.scss';
 
 interface PaginationProps {
@@ -6,8 +8,20 @@ interface PaginationProps {
 }
 
 function Pagination({ totalResults }: PaginationProps) {
-  const [totalFilmsPage] = useState(Math.floor(totalResults / 10));
-  const activePage = 1;
+  const NUMBER_CARD_ON_PAGE = 10;
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+  const [activePage, setActivePage] = useState(1);
+
+  const [totalFilmsPage, setTotalFilmsPage] = useState(
+    Math.ceil(totalResults / NUMBER_CARD_ON_PAGE)
+  );
+
+  const handleSetPage = (page: number) => {
+    setActivePage(page);
+    searchParams.set('page', page.toString());
+    navigate(`?${searchParams.toString()}`);
+  };
 
   const renderPages = () => {
     const pages = [];
@@ -36,14 +50,31 @@ function Pagination({ totalResults }: PaginationProps) {
     }
 
     return pages.map((page) => (
-      <li
-        className={`pagination__item ${activePage === page ? ' active' : ''}`}
-        key={`page-${page}`}
-      >
-        {page}
+      <li className="pagination__item" key={`page-${page}`}>
+        <button
+          type="button"
+          className={
+            activePage === page
+              ? 'pagination__button active'
+              : 'pagination__button'
+          }
+          onClick={() => handleSetPage(page)}
+        >
+          {page}
+        </button>
       </li>
     ));
   };
+
+  useEffect(() => {
+    setTotalFilmsPage(Math.floor(totalResults / NUMBER_CARD_ON_PAGE) + 1);
+    handleSetPage(1);
+  }, [totalResults]);
+
+  useEffect(() => {
+    searchParams.set('page', '1');
+    navigate(`?${searchParams.toString()}`);
+  }, []);
 
   return <ul className="pagination">{renderPages()}</ul>;
 }
