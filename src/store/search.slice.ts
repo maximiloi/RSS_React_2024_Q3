3b3c4c21f09187d69b5c1009c8c1a55e641d53dd
maximiloi <maximiloi@gmail.com>
2024-07-22 22:11:13 +0300

    feat: add reducer updateActivePage

--- src/store/search.slice.ts
+++ src/store/search.slice.ts
@@ -1,12 +1,14 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface SearchState {
+  activePage: number;
   page: string;
   searchWord: string;
   totalResults: string;
 }
 
 const initialState: SearchState = {
+  activePage: 1,
   page: '1',
   searchWord: 'star wars',
   totalResults: '0',
@@ -16,6 +18,9 @@ const searchSlice = createSlice({
   name: 'search',
   initialState,
   reducers: {
+    updateActivePage(state, action: PayloadAction<number>) {
+      return { ...state, activePage: action.payload };
+    },
     updatePage(state, action: PayloadAction<string>) {
       return { ...state, page: action.payload };
     },
