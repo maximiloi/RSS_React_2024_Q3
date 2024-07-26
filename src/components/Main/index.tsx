2f0a35df81c33dff6b51fa6fba93674dbaf9c020
maximiloi <maximiloi@gmail.com>
2024-07-26 21:18:29 +0300

    refactor: change first load items

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,41 +1,11 @@
-import { useEffect } from 'react';
-import { Outlet, useSearchParams } from 'react-router-dom';
-import useLocalStorage from '../../hooks/useLocalStorage';
-import useActions from '../../hooks/useActions';
+import { Outlet } from 'react-router-dom';
+
 import Pagination from '../Pagination';
 import Card from '../MoviesList';
 
 import './style.scss';
 
 const Main: React.FC = () => {
-  const [searchParams] = useSearchParams();
-  const [searchTermLS] = useLocalStorage('');
-  const { updateActivePage, updateSearchWord, updatePage } = useActions();
-
-  useEffect(() => {
-    const termFromParams = searchParams.get('search');
-    const pageFromParams = searchParams.get('page');
-
-    if (termFromParams) {
-      updateSearchWord(termFromParams);
-    }
-
-    if (pageFromParams) {
-      updatePage(pageFromParams);
-      updateActivePage(parseInt(pageFromParams, 10));
-    }
-
-    if (!termFromParams && !pageFromParams) {
-      updateSearchWord(searchTermLS);
-    }
-  }, [
-    searchParams,
-    searchTermLS,
-    updateActivePage,
-    updatePage,
-    updateSearchWord,
-  ]);
-
   return (
     <main>
       <div className="main__container">
--- src/components/MoviesList/index.tsx
+++ src/components/MoviesList/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { useSearchParams } from 'react-router-dom';
 import useActions from '../../hooks/useActions';
 import { RootState } from '../../store/store';
 import { useGetMoviesQuery } from '../../store/api';
@@ -7,21 +8,27 @@ import Spinner from '../Spinner';
 import MovieCard from '../MovieCard';
 
 const MoviesList: React.FC = () => {
+  const [searchParams] = useSearchParams();
   const { updateTotalResults } = useActions();
   const searchWord = useSelector(
     (state: RootState) => state.search?.searchWord
   );
   const page = useSelector((state: RootState) => state.search?.page);
 
+  const urlSearchTerm = searchParams.get('search') || searchWord;
+  const urlPage = searchParams.get('page') || page;
+
   const fetchData = useGetMoviesQuery({
-    searchTerm: searchWord,
-    page,
+    searchTerm: urlSearchTerm,
+    page: urlPage,
   });
   const { data, isLoading } = fetchData;
 
   useEffect(() => {
     if (!data) return;
     updateTotalResults(data.totalResults);
+    // if (!searchParams.get('page')) return;
+    // updateActivePage(parseInt(searchParams.get('page'), 10));
   }, [data, updateTotalResults]);
 
   const renderMovies = () => {
